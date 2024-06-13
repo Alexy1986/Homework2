b451ead60efe956dd86adfe779cae55649dcc422
Alexy1986 <alexyalexy12@gmail.com>
2024-06-13 20:22:23 +0500

    Add files via upload

--- Homework2.py
+++ Homework2.py
@@ -1,9 +1,7 @@
 Homework= 12
 Hours= 1.5
 Name_kyrs= "Python"
-Time_for_task= 0.125
+Time_for_task= Hours/Homework
 Time= "часа"
-Time_min=60*Time_for_task
-Time_sec=60*Time_min
-print(Name_kyrs+',',"всего задач:"+str(Homework)+",", "затрачено часов:" +str(Hours)+",", "среднее время выполнения", Time_for_task,Time, 'или', Time_min, "минут")# с двумя переменными
-print(Name_kyrs+',',"всего задач:"+str(Homework)+",", "затрачено часов:" +str(Hours)+",", "среднее время выполнения", Time_for_task,"часа", 'или', Time_sec, "секунд") # с одной переменной
+print(Name_kyrs+',',"всего задач:"+str(Homework)+",", "затрачено часов:" +str(Hours)+",", "среднее время выполнения", Time_for_task,Time)
+
